2d28981a6db3bf04316da81d5bda8b72f52c3cc7
JacksonTucker <jtuc3124@uni.sydney.edu.au>
2025-04-10 13:47:24 +1000

    Peak view added

--- sl_test.py
+++ sl_test.py
@@ -14,6 +14,10 @@ n_pi = st.radio("n - Number of π phase shifts", [0,1,3,5,7], index=0, horizonta
 # Choice of X-axis (removed in favour of double labelling)
 #xoptions = ["GHz","pm"]
 #xchoice = st.radio("X axis",xoptions, horizontal = True)
+if n_pi>0:
+    view = st.radio("View", ['Wide','Peak'], horizontal = True)
+else:
+    view = 'Wide'
 
 # Plotting inputs
 cwl_m = cwl*1E-9
@@ -47,12 +51,19 @@ def g2p(ghz):
 fbgdB = func.fbg_spectrum_shifts_dB(xs,cwl,l_mm,n_pi,kappa)
 pms = (xs-cwl_m)*1E12
 ghz = func.xs2ghz(xs,cwl_m)
+PBwig, max_suppression, min_detune_pm, min_detune_ghz, PBwidth_pm, PBwidth_ghz = func.fbg_characteristics(xs,fbgdB,cwl,n_pi)
 
 fig, ax = plt.subplots()
 ax.plot(pms,fbgdB)
 ax.set_xlabel('Wavelength Detuning [pm]')
 ax.set_ylabel('Transmission [dB]')
 ax.set_title(f'{n_pi}πFBG spectrum')
+if view == 'Peak' and n_pi>0:
+    if PBwidth_pm == 'N/A':
+        ax.set_xlim([-5,5])
+    else:
+        ax.set_xlim([-2*PBwidth_pm,2*PBwidth_pm])
+        
 secax = ax.secondary_xaxis('top', functions=(p2g,g2p))
 secax.set_xlabel('Frequency Detuning [GHz]')
 plt.show()
@@ -60,7 +71,6 @@ plt.show()
 st.pyplot(fig)
 
 # Characteristics readouts
-PBwig, max_suppression, min_detune_pm, min_detune_ghz, PBwidth_pm, PBwidth_ghz = func.fbg_characteristics(xs,fbgdB,cwl,n_pi)
 st.write(f'Max suppression: {np.round(max_suppression,2)} dB')
 st.write(f'Minima location: {np.round(min_detune_pm,4)} pm, {np.round(min_detune_ghz,4)} GHz detuning')
 if n_pi > 0:
